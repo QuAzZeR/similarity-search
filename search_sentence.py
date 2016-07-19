92468665f1b96580ae42e4b1c7e7bef9b794d207
QuAzZeR <natsamiss@gmail.com>
2016-07-19 09:53:15 +0200

    fixed calculate distance

--- search_sentence.py
+++ search_sentence.py
@@ -38,6 +38,8 @@ def cosine_distance(q1,q2):
     # print qgramI
     # print len(q1),len(q2)
     # print math.sqrt(len(q1)*len(q2))
+    # try:
+    # print(len(q1),len(q2))
     try:
         distance =  1 - qgramI/math.sqrt(len(q1)*len(q2))
         return distance
@@ -58,7 +60,7 @@ def read_from_file(path_of_file):
 
 
 
-def print_(cosine_dist,topn = 3):
+def print_(cosine_dist,topn = 1):
     for i in range(topn):
         print (cosine_dist[i])
 
@@ -70,11 +72,19 @@ def read_from_file(path_of_file):
         list_of_sentences_encoding.append(ast.literal_eval(i))
     return list_of_sentences_encoding
 
-def cal_distant_parallel(args):
+def cal_distant_parallel(args):#(search_sentence_encoding,list_of_sentences_encoding,ans):
+    # print (args)
+    # for i in range(len(list_of_sentences_encoding)):
+    #     # print(list_of_sentences_encoding[i][3])
+    #     # print list_of_sentences_encoding
+    #     print (ans[i])
+    #     ans[i] = (list_of_sentences_encoding[i][2],1-cosine_distance(search_sentence_encoding,list_of_sentences_encoding[i][3]))
+    #     print (ans[i])
 
-    parallel_dist = 1-cosine_distance(args[1],args[1][3])
-    # if args[0] == 1:
-    word_distance1.put((args[1][2],parallel_dist))
+    parallel_dist = 1-cosine_distance(args[0],args[1][3])
+    # print (parallel_dist)
+    # # if args[0] == 1:
+    # word_distance1.put((args[1][2],parallel_dist))
     return (args[1][2],parallel_dist)
 
     # else:
@@ -83,7 +93,7 @@ def cal_distant_parallel(args):
 def main():
     # print(os.getpid())
     # path_of_file = '../Data/pg25990.txt'
-    list_of_sentences_encoding = read_from_file('./test_prepare5000')
+    list_of_sentences_encoding = read_from_file('./test_prepare700')
     # print (len(list_of_sentences_encoding))
     pool = mp.Pool(mp.cpu_count())
     number_of_sentence = len(list_of_sentences_encoding)
@@ -92,22 +102,32 @@ def main():
     search_sentence = input()
     sentence, sentence_with_word = get_word_from_document(search_sentence)
     # print(sentence,sentence_with_word)
+    # time.sleep(1.0)
     search_sentence_with_encoding = find_cluster(sentence_with_word[0],4)
+    # print (list_of_sentences_encoding[0])
     list_for_parallel = [(search_sentence_with_encoding,i) for i in list_of_sentences_encoding]
 
-    # print (type())
-    # list_for_parallel2 = [(2,search_sentence_with_encoding,i) for i in list_of_sentences_encoding[len_1:]]
-    # pool.map(cal_distant_parallel,list_for_parallel2)
     start =  time.time()
-    # pool.map(cal_distant_parallel,list_for_parallel)
-    # list_of_distance = [word_distance1.get() for i in range(number_of_sentence)]
+    # N = 4
+    # p = []
+    #
+    # for i in range(1,N):
+    #     p.append(mp.Process(target = cal_distant_parallel,args =(search_sentence,list_of_sentences_encoding[(i-1)*int(number_of_sentence/N):(i)*int(number_of_sentence/N)],list_of_distance[(i-1)*int(number_of_sentence/N):(i)*int(number_of_sentence/N)],)))
+    # p.append(mp.Process(target = cal_distant_parallel,args =(search_sentence,list_of_sentences_encoding[(i-1)*int(number_of_sentence/N):],list_of_distance[(i-1)*int(number_of_sentence/N):],)))
+    # print ((len(p)))
+    # for i in p:
+    #     i.start()
+    #     i.join()
+    # print
     list_of_distance = [cal_distant_parallel(i) for i in list_for_parallel]
     end = time.time()
-    list_of_distance = sorted(list_of_distance,key = lambda x: x[0])
+    # print(list_of_distance[0])
+    list_of_distance = sorted(list_of_distance,key = lambda x: -x[1])
 
+    # print(list_of_distance[0])
     #     list_of_distance.append(x)
     print(end-start)
-    print(len(list_of_distance))
+    print_(list_of_distance,3)
     # for i in list_of_distance:
     #     print (i)
 
